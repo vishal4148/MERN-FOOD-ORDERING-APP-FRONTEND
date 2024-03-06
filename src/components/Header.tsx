f68d6b6caba5ee99604ca3bedf630ef2d2b463fc
Vishal boraste <vishalboraste321@gmail.com>
2024-03-06 14:22:30 +0530

    Header file updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
 import MainNav from "./MainNav";
-//i am vishal boraste
 const Header = () => {
     return(
         <div className="border-b-2 border-b-orange-500 py-6">
